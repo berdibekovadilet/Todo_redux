1b46c53581f8cf4b019fb4864ce20bc989953f76
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-04-07 00:28:42 +0600

    added redux

--- src/App.js
+++ src/App.js
@@ -2,70 +2,7 @@ import { useReducer } from "react";
 import { Button, Divider, List, Paper, Tab, Tabs } from "@mui/material";
 import AddFeild from "./components/AddFeild";
 import Item from "./components/Item";
-
-function reducer(state, action) {
-  if (action.type === "ADD_TASK") {
-    return {
-      ...state,
-      tasks: [
-        ...state.tasks,
-        {
-          id: state.tasks[state.tasks.length - 1].id + 1,
-          text: action.payload.text,
-          completed: action.payload.checked,
-        },
-      ],
-    };
-  }
-
-  if (action.type === "REMOVE_TASK") {
-    return {
-      ...state,
-      tasks: state.tasks.filter((obj) => obj.id !== action.payload),
-    };
-  }
-
-  if (action.type === "TOGGLE_COMPLETE") {
-    return {
-      ...state,
-      tasks: state.tasks.map((obj) => {
-        if (obj.id === action.payload) {
-          return {
-            ...obj,
-            completed: !obj.completed,
-          };
-        }
-        return obj;
-      }),
-    };
-  }
-
-  if (action.type === "COMPLETE_ALL") {
-    return {
-      ...state,
-      tasks: state.tasks.map((obj) => ({
-        ...obj,
-        completed: true,
-      })),
-    };
-  }
-
-  if (action.type === "CLEAR") {
-    return {
-      ...state,
-      tasks: [],
-    };
-  }
-
-  if (action.type === "SET_FILTER") {
-    return {
-      ...state,
-      filterBy: action.payload,
-    };
-  }
-
-  return state;
-}
+import { useSelector, useDispatch } from "react-redux";
 
 const filterIndex = {
   all: 0,
@@ -74,31 +11,8 @@ const filterIndex = {
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {
-    filterBy: "all",
-    tasks: [
-      {
-        id: 1,
-        text: "Первая задача",
-        completed: false,
-      },
-      {
-        id: 2,
-        text: "Вторая задача",
-        completed: true,
-      },
-      {
-        id: 3,
-        text: "Задача 3",
-        completed: false,
-      },
-      {
-        id: 4,
-        text: "Задача 4 ",
-        completed: true,
-      },
-    ],
-  });
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   const addTask = (text, checked) => {
     dispatch({
